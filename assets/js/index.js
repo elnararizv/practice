0626acdfd7596bd8eec976be7b8d5d992439d912
elnara002 <6fdtxsg@code.edu.az>
2024-03-14 23:07:24 +0400

    ppractice 14/03

---
+++ assets/js/index.js
@@ -0,0 +1,76 @@
+// 1-dən 100-ə qədər olan bütün ədədləri ekrana çap edin.
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(i)
+// }
+
+// ---------------------------------------------------------------
+// 1-dən 100-ə qədər olan bütün cütləri ekrana çap edin.
+//      for (let i = 0; i < 100; i++) {
+//         if(i % 2 == 0){
+//             console.log(i)
+//         }
+// }
+
+// ---------------------------------------------------------------
+// 1-dən 100-ə qədər olan bütün tək ədədlərin cəmini tapın və ekrana çap edin.
+//   let sum=0
+//   for (let i = 0; i < 100; i++) {
+//    if(i%2==1){
+//     sum+=i
+//    }
+//   }console.log(sum)
+
+// ---------------------------------------------------------------
+// 1-dən 100-ə qədər olan bütün ədədlərin kvadratlarını ekrana çap edin.
+// for (let i = 0; i <100; i++) {
+//    console.log(i**2)
+// }
+
+// ---------------------------------------------------------------
+// 1-dən 100-ə qədər olan bütün ədədlərin kublarını ekrana çap edin.
+// for (let i = 0; i <100; i++) {
+//    console.log(i**3)
+// }
+
+// ---------------------------------------------------------------
+// 1-dən 100-ə qədər olan bütün cütlərin cəmini tapın və ekrana çap edin.
+// let sum=0
+// for (let i = 0; i < 100; i++) {
+//     if(i%2==0){
+//         sum+=i
+//     } 
+// }console.log(sum)
+
+// ---------------------------------------------------------------
+// 1-dən 100-ə qədər olan bütün tək ədədlərin hasilini tapın və ekrana çap edin.
+// let result=1
+// for (let i = 1; i < 100; i++) {
+//    if(i%2==1){
+//     result*=i
+//    }
+// }console.log(result)
+
+// ---------------------------------------------------------------
+// 1-dən 100-ə qədər olan bütün cütlərin hasilini tapın və ekrana çap edin.
+// let result=1
+// for (let i = 1; i < 100; i++) {
+//    if(i%2==0){
+//     result*=i
+//    }
+// }console.log(result)
+// ---------------------------------------------------------------
+// 1-dən 100-ə qədər olan bütün ədədlərin toplamını tapın və ekrana çap edin.
+// let sum=0
+// //   for (let i = 0; i < 100; i++) {
+// //     sum+=i
+// //   }console.log(sum)
+
+// ---------------------------------------------------------------
+// 1-dən 100-ə qədər olan bütün ədədlərin orta qiymətini tapın və ekrana çap edin.
+let sum=0
+let count=0
+  for (let i = 0; i < 100; i++) {
+    count++
+    sum+=i
+  }console.log(sum/count)
